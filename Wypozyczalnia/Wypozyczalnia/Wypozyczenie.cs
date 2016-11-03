63b89cb8d7505d951eed3038d3adb26a3b4171de
Kordian <kordiankurdziel@gmail.com>
2016-11-03 08:00:11 +0100

    Dodana klasa Wypozyczenie.cs

---
+++ Wypozyczalnia/Wypozyczalnia/Wypozyczenie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wypozyczalnia {
+    class Wypozyczenie {
+        private Klient klient;
+        private Film film;
+        private DateTime dataOd;
+        private DateTime dataDo;
+        private double kwota;
+
+        public Wypozyczenie(Klient klient, Film film, DateTime dataOd, DateTime dataDo) {
+            this.klient = klient;
+            this.film = film;
+            this.dataOd = dataOd;
+            this.dataDo = dataDo;
+        }
+
+        private void obliczKwote() {
+
+        }
+    }
+}
